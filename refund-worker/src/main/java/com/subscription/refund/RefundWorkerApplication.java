4731235a88503f7010ecf2d238dac337293e3818
akshit2941 <sainiakshit2941@gmail.com>
2025-11-16 14:15:52 +0530

    initial project setup

---
+++ refund-worker/src/main/java/com/subscription/refund/RefundWorkerApplication.java
@@ -0,0 +1,13 @@
+package com.subscription.refund;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefundWorkerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefundWorkerApplication.class, args);
+	}
+
+}
---
+++ refund-worker/src/test/java/com/subscription/refund/RefundWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subscription.refund;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RefundWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
